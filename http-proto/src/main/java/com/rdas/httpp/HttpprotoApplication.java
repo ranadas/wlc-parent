737903e2a2b10d010ec0316dd90c9dcfb46c407e
ranadas <rana.pratap.das@gmail.com>
2021-03-20 17:59:20 +0000

    adding  a http war web service

---
+++ http-proto/src/main/java/com/rdas/httpp/HttpprotoApplication.java
@@ -0,0 +1,13 @@
+package com.rdas.httpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpprotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpprotoApplication.class, args);
+	}
+
+}
